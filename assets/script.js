a1ca044fb2336d5d94b35f3491d2093789bdc28f
Annie Rizvi <annie.rizvi@paymentsense.com>
2023-09-24 19:36:38 +0100

    Update Js To For Loop

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+var topics = ["HTML", "CSS", "Git", "JavaScript"];
+
+/*if (topic === "HTML") {
+  console.log("Let's study HTML!");
+} else if (topic === "CSS") {
+  console.log("Let's study CSS!");
+} else if (topic === "Git") {
+  console.log("Let's study Git!");
+} else if (topic === "JavaScript") {
+  console.log("Let's study JavaScript!");
+} else {
+  console.log("Please try again!");
+}
+*/
